14b9da189ae8884f75db465a0a1ede4b1ac568ea
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-25 12:46:02 -0600

    working movie search

--- app/controllers.js
+++ app/controllers.js
@@ -6,13 +6,11 @@ app.controller('homeController', ["$scope", "$http", "$stateParams", function($s
     $scope.view.search = function (string) {
       $http.get('https://www.omdbapi.com/?s=' + string).then(function(data) {
         $scope.view.results = data.data.Search
-        console.log($scope.view.results)
         $scope.view.searchText = ''
       })
     }
     $http.get('https://www.omdbapi.com/?s=' + $stateParams.title).then(function(data) {
       $scope.view.results = data.data.Search
-      console.log($scope.view.results)
       // $scope.view.searchText = ''
     })
 }])
@@ -22,7 +20,6 @@ app.controller('nextController', ['$scope','$stateParams','$http', function($sco
     $state.go('next')
   }
     $http.get('https://www.omdbapi.com/?t=' + $stateParams.movieTitle).then(function(data){
-      console.log(data)
       $scope.view = {}
       $scope.view.movie = data.data
     })
